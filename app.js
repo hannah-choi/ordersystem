dc477aa3f1758a80a0393f479b7d1f3caa6eff13
hannah-developer <hannahchoi.developer@gmail.com>
2020-07-19 22:12:05 +0900

    1. added function for listRender / 2. CSS refactoring

--- app.js
+++ app.js
@@ -3,27 +3,119 @@
 
 const app = {
     data: [
-        //메뉴 2개 상품은 2개
+        //메뉴 3개, 상품 최소 2-3개, 이미지 로컬로 준비해서 images에 넣을것 
         {
-            menu: "Loose Tea",
+            menu: "LOOSE TEA",
             list: [{
                 name: "Alishan Jingsyuan",
-                price: 20.00
+                price: 18.00,
+                category: 'loosetea',
+                id: 0
+
             },
             {
-                name: "Si ji chun",
-                price: 30.00
+                name: "Lishan Oolong",
+                price: 45.00,
+                category: 'loosetea',
+                id: 1
+            },
+            {
+                name: "Tie Guan Yin",
+                price: 25.00,
+                category: 'loosetea',
+                id: 2
+            },
+            {
+                name: "Roasted Shanlinsi Oolong",
+                price: 28.00,
+                category: 'loosetea',
+                id: 3
+            },
+            {
+                name: "Premium Jasmine Green Tea",
+                price: 15.00,
+                category: 'loosetea',
+                id: 4
+            },
+            {
+                name: "Oriental Beauty",
+                price: 24.00,
+                category: 'loosetea',
+                id: 5
+            },
+            {
+                name: "Aged Tie Guan Yin",
+                price: 35.00,
+                category: 'loosetea',
+                id: 6
+            },
+            {
+                name: "Aged Dongding Oolong",
+                price: 42.00,
+                category: 'loosetea',
+                id: 7
             }]
+
         },
         {
-            menu: "Tea bag",
+            menu: "TEA BAG",
             list: [{
-                name: "Alishan Jingsyuan",
-                price: 20.00
+                name: "Si Ji Chun",
+                price: 5.00,
+                category: 'teabag',
+                id: 0
             },
             {
-                name: "Si ji chun",
-                price: 30.00
+                name: "Alishan Jingsyuan",
+                price: 5.00,
+                category: 'teabag',
+                id: 1
+            },
+            {
+                name: "Lightly Roasted DongDing Oolong",
+                price: 5.00,
+                category: 'teabag',
+                id: 2
+            },
+            {
+                name: "Tie Guan Yin",
+                price: 5.00,
+                category: 'teabag',
+                id: 3
+            },
+            {
+                name: "Premium Jasmine Green Tea",
+                price: 5.00,
+                category: 'teabag',
+                id: 4
+            },
+            {
+                name: "Guei Fei Tea",
+                price: 5.00,
+                category: 'teabag',
+                id: 5
+            }]
+        },
+
+        {
+            menu: "GIFT BOX",
+            list: [{
+                name: "Alishan Oolong + Premium Jasmine Green Tea",
+                price: 40.00,
+                category: 'giftbox',
+                id: 0
+            },
+            {
+                name: "Si ji Chun + Lightly roasted Oolong Tea",
+                price: 30.00,
+                category: 'giftbox',
+                id: 1
+            },
+            {
+                name: "Lightly Roasted Oolong Tea + Premium Jasmine Green Tea",
+                price: 35.00,
+                category: 'giftbox',
+                id: 2
             }]
         }
 
--- menuList.js
+++ menuList.js
@@ -1,27 +1,64 @@
+let $nav = document.querySelector('.ul_nav'); //nav_ul
+
 const menuList = {
 
     setState(data) { //app.js에서 데이터를 받아서 데이터를 갱신해주는 용도 
         this.data = data;
         this.navRender();
         this.listRender();
+
     },
 
     navRender() { //html요소를 직접적으로 만드는 함수
-        $list = document.querySelector('.ul_nav'); //nav_ul
+
         let contents = "";
 
 
         for (let i = 0; i < this.data.length; i++) {
 
-            contents += `<li>${this.data[i].menu}</li>` //가공해서 넣어주기
-
+            contents += `<li data-index="${i}">${this.data[i].menu}</li>` //가공해서 넣어주기
         }
 
-        $list.innerHTML = contents;
+        $nav.innerHTML = contents;
     },
 
     listRender() {
-        //리스트 함수를 직접 구현할 것
-    }
+        let $menuList = document.querySelector('.menu_list');
+        let contents = "";
+        let index = 0; //클릭한 li의 인덱스
 
+        // for (let i = 0; i < this.data.length; i++) {
+        //     let menuListLength = this.data[i].list.length;
+        //     //console.log(menuListLength)
+
+        let menuList = this.data[index].list.length;
+        console.log(menuList);
+        for (let i = 0; i < menuList; i++) {
+            console.log(this.data[index].list[i]);
+
+            const list = this.data[index].list[i]
+            const fileName = list.name.split(' ').join('').toLowerCase();
+            //const fileName = fileNameStr.toLowerCase();
+
+            console.log(fileName)
+
+            contents += `<li><img
+            src="images/${list.category}_${fileName}.webp"> 
+            <span class="prod_name">${list.name}</span><span class="prod_price">£ ${list.price.toFixed(2)}</span></li>` //가공해서 넣어주기
+
+
+        }
+
+
+
+
+        //contents += `<li data-index="${i}">${this.data[i].menu}</li>` //가공해서 넣어주기
+
+        $menuList.innerHTML = contents;
+
+
+
+
+        //
+    }
 }
