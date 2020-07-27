0596312b3d88348720b2f86ce3f843818d16cb35
hannah-developer <hannahchoi.developer@gmail.com>
2020-07-27 20:46:39 +0900

    modified deleteClick(),   menuItemClick() / Created changeSelectBox()

--- script/app.js
+++ script/app.js
@@ -9,8 +9,9 @@ class App {
         this.$app = document.querySelector('#app') //선언하는 부분이므로 constructor안에 넣는다 
         this.data = menuData
         this.orderData = []
-        this.sumValue = [];
         this.$total = document.querySelector('.total')
+        this.$table = document.querySelector('.order_table')
+        this.countClick = 0;
 
     }
     init() {
@@ -34,18 +35,29 @@ class App {
                 case 'TD':
                     alert('quantity')
                     break;
-                case 'total':
-                    alert('pay button')
+                case 'pay_button':
+                    alert('!!!')
+                    this.payButtonClick(target);
                     break;
                 default:
                     return;
             }
 
-        }
+        })
 
-        );
+        this.$app.addEventListener('change', ({ target }) => {
+            switch (target.dataset.key) {
+                case 'selectbox':
+                    this.changeSelectBox(target);
+                    break;
+
+            }
+        })
+
 
     }
+
+
 
 
     selected(target) {
@@ -62,33 +74,45 @@ class App {
         //this.sumValue = this.orderData.map(orderData => orderData.price)
         if (this.selectedIndex(target) === -1) { //숙제 : find함수 말고 다른애로 변경
             const selected = this.selected(target)
-            selected.count = 0;
+            selected.count = 1;
             this.orderData.push(selected)
-            this.sumValue.push(selected.price)
             this.orderList.setState(this.orderData, selected.count)
             //this.menuData.push({...selected, count:0})
         } else {
+            //console.log(this.selectedIndex(target))
             this.orderData[this.selectedIndex(target)].count++;
-            this.sumValue.push(this.orderData[this.selectedIndex(target)].price)
             this.orderList.setState(this.orderData, this.orderData[this.selectedIndex(target)].count)
             //this.orderList.makeSelect(this.orderData[selectedIndex].count)
         }
         //this.orderList.setState(this.orderData, this.orderData[selectedIndex].count);
-        this.orderList.totalRender(this.sumValue.reduce((a, b) => a + b, 0).toFixed(2));
+
         //undefined가 나오면 push를 해주고 아니면 이미 있다는 뜻이니까 push를 해줄 필요가 없음
+
+        console.log()
 
     }
 
-    deleteClick() {
-        this.orderData.splice(this.selectedIndex(), 1)
-        this.sumValue.splice(this.selectedIndex(), 1)
+    deleteClick(target) {
+        this.orderData.splice(this.selectedIndex(target), 1)
         this.orderList.setState(this.orderData);
-        this.orderList.totalRender(this.sumValue.reduce((a, b) => a + b, 0).toFixed(2));
+
     }
 
     allClearClick() {
         this.orderData.splice(0,)
         this.sum.splice(0,)
+        this.orderList.setState(this.orderData)
+    }
+
+    payButtonClick(target) {
+
+
+    }
+
+
+    changeSelectBox(target) {
+        let dataindex = target.parentElement.parentElement.parentElement.dataset.index;
+        this.orderData[dataindex].count = target.value;
         this.orderList.setState(this.orderData)
     }
 
--- script/orderList.js
+++ script/orderList.js
@@ -6,36 +6,39 @@ class OrderList {
         this.$total = document.querySelector('.total')
     }
 
-    setState(data, count) {
+    setState(data) {
         this.data = data;
-        this.makeSelect(count);
-        this.orderRender(count);
-        this.totalRender('0.00');
+        this.orderRender();
+
     }
 
     makeSelect(count) { //카운트 파라메터를 받아서 i랑 비교할 것 
         let options = ""
         for (let i = 1; i <= 10; i++) {
-            options += `<option value="${i}" ${i === count ? "selected" : ""}>${i}</option>`
+            options += `<option value="${i}" ${i === +count ? "selected" : ""}>${i}</option>`
             //options += `<option value="${i}" ${i === count ? "selected" : ""}>${i}</option>`
         }
-        let selectbox = `<select name="quantity" id="quantity">${options}</select>`
+        let selectbox = `<select name="quantity" id="quantity" data-key="selectbox">${options}</select>`
         return selectbox
     }
 
-    orderRender(count) {
+    orderRender() {
         let contents = "";
+        let itemTotal = 0;
         for (let i = 0; i < this.data.length; i++) {
+            itemTotal += this.data[i].price * this.data[i].count;
             contents += `<tr data-key="orderedItem" data-index="${i}">
             <td class="order_prod_name">${this.data[i].name}</td>
             <td class="order_quantity"><label for="quantity">
-                ${this.makeSelect(count)}
+                ${this.makeSelect(this.data[i].count)}
                     </td>
-            <td class="order_price">£ ${this.data[i].price.toFixed(2)}</td>
+            <td class="order_price">£ ${(this.data[i].price * this.data[i].count).toFixed(2)}</td>
             <td class="order_delete"><input type="button" class="order_delete" value="×" data-key="deleteItem"></td>
         </tr>`
+
         }
         this.$table.innerHTML = contents;
+        this.totalRender(itemTotal.toFixed(2));
 
     }
 
