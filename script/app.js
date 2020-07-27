1962b4443d0d342653d349c39c5414d1900650c2
hannah-developer <hannahchoi.developer@gmail.com>
2020-07-27 19:35:52 +0900

    created selected(), selectedIndex(), menuItemClick(), deleteClick()

--- script/app.js
+++ script/app.js
@@ -17,42 +17,19 @@ class App {
         this.menuList.setState(this.data, 0)
         this.orderList.setState(this.orderData)
         this.$app.addEventListener('click', ({ target }) => {
-            let selected = this.data[this.menuList.selectTab].list[target.dataset.index];
-            let selectedIndex = this.orderData.findIndex(data => data === selected) //위와 어떻게 다른가?
 
             switch (target.dataset.key) { //스위치는 괄호업ㅅ이
                 case 'navItem':
                     this.menuList.setState(this.data, target.dataset.index)
                     break;
                 case 'menuItem':
-                    //this.orderData//지금은 비어있는배열에 push해서 상품을 넣어준다
-                    //this.sumValue = this.orderData.map(orderData => orderData.price)
-                    if (selectedIndex === -1) { //숙제 : find함수 말고 다른애로 변경
-                        selected.count = 0;
-                        this.orderData.push(selected)
-                        this.sumValue.push(selected.price)
-                        this.orderList.setState(this.orderData, selected.count)
-                        //this.menuData.push({...selected, count:0})
-                    } else {
-                        this.orderData[selectedIndex].count++;
-                        this.sumValue.push(this.orderData[selectedIndex].price)
-                        this.orderList.setState(this.orderData, this.orderData[selectedIndex].count)
-                        //this.orderList.makeSelect(this.orderData[selectedIndex].count)
-                    }
-                    //this.orderList.setState(this.orderData, this.orderData[selectedIndex].count);
-                    this.orderList.totalRender(this.sumValue.reduce((a, b) => a + b, 0).toFixed(2));
-                    //undefined가 나오면 push를 해주고 아니면 이미 있다는 뜻이니까 push를 해줄 필요가 없음
+                    this.menuItemClick(target)
                     break;
                 case 'deleteItem':
-                    this.orderData.splice(selectedIndex, 1)
-                    this.sumValue.splice(selectedIndex, 1)
-                    this.orderList.setState(this.orderData);
-                    this.orderList.totalRender(this.sumValue.reduce((a, b) => a + b, 0).toFixed(2));
+                    this.deleteClick(target)
                     break;
                 case 'icon_trash': // 숙제(옵션) 삭제되는 기능까지 구현
-                    this.orderData.splice(0,)
-                    this.sum.splice(0,)
-                    this.orderList.setState(this.orderData)
+                    this.allClearClick(target);
                     break;
                 case 'TD':
                     alert('quantity')
@@ -64,16 +41,56 @@ class App {
                     return;
             }
 
-
-
-
-
         }
 
         );
 
     }
 
+
+    selected(target) {
+        return this.data[this.menuList.selectTab].list[target.dataset.index]
+    }
+
+    selectedIndex(target) {
+        return this.orderData.findIndex(data => data === this.selected(target))
+    }
+
+
+    menuItemClick(target) {
+        //this.orderData//지금은 비어있는배열에 push해서 상품을 넣어준다
+        //this.sumValue = this.orderData.map(orderData => orderData.price)
+        if (this.selectedIndex(target) === -1) { //숙제 : find함수 말고 다른애로 변경
+            const selected = this.selected(target)
+            selected.count = 0;
+            this.orderData.push(selected)
+            this.sumValue.push(selected.price)
+            this.orderList.setState(this.orderData, selected.count)
+            //this.menuData.push({...selected, count:0})
+        } else {
+            this.orderData[this.selectedIndex(target)].count++;
+            this.sumValue.push(this.orderData[this.selectedIndex(target)].price)
+            this.orderList.setState(this.orderData, this.orderData[this.selectedIndex(target)].count)
+            //this.orderList.makeSelect(this.orderData[selectedIndex].count)
+        }
+        //this.orderList.setState(this.orderData, this.orderData[selectedIndex].count);
+        this.orderList.totalRender(this.sumValue.reduce((a, b) => a + b, 0).toFixed(2));
+        //undefined가 나오면 push를 해주고 아니면 이미 있다는 뜻이니까 push를 해줄 필요가 없음
+
+    }
+
+    deleteClick() {
+        this.orderData.splice(this.selectedIndex(), 1)
+        this.sumValue.splice(this.selectedIndex(), 1)
+        this.orderList.setState(this.orderData);
+        this.orderList.totalRender(this.sumValue.reduce((a, b) => a + b, 0).toFixed(2));
+    }
+
+    allClearClick() {
+        this.orderData.splice(0,)
+        this.sum.splice(0,)
+        this.orderList.setState(this.orderData)
+    }
 
 }
 
