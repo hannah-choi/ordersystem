3d6b92acbaddd98e48631c804d54f58759b096be
hannah-developer <hannahchoi.developer@gmail.com>
2021-03-30 23:37:51 +0100

    modified prepared queries

--- routes/cart.js
+++ routes/cart.js
@@ -1,33 +1,45 @@
 var express = require('express');
+var mysql      = require('mysql');
 var router = express.Router();
 const db = require("../db.js");
 
 router.get("/", (req, res) => { //처음 로딩시에 카트(에 아이템이 있다면) 보여주기
+    const columns = mysql.raw('cartData.id as orderId, prodName, price, category, count, menuData.Id as id')
+    const condition = mysql.raw('menuData.id = cartData.prodId')
+    const sql = `SELECT ? FROM menuData JOIN cartData on ?`
+    const preparedQuery = mysql.format(sql, [columns, condition]);
     db.query(
-        `SELECT cartData.id as orderId, prodName, price, category, count, menuData.Id as id FROM menuData JOIN cartData on menuData.id = cartData.prodId`, (err, rows) => {
+        preparedQuery, (err, rows) => {
             res.send(rows)
         }
     )
 })
 
 router.delete("/all", (req, res) => {
-    db.query(`DELETE FROM cartData`, (err, rows) => {
+    const table = mysql.raw('cartData')
+    db.query(`DELETE FROM cartData`, [table], (err, rows) => {
         res.send({ status: 200 });
     });
 });
 
 router.delete("/", (req, res) => {
-    db.query(`DELETE FROM cartData where prodId = ${parseInt(req.query.id)}`, (err, rows) => {
+    function getId(){return parseInt(req.query.id)}
+    const query = db.query(`DELETE FROM cartData where prodId = ?`, [getId()], (err, rows) => {
         res.send({ status: 200 });
     });
+    console.log(query.sql)
 });
 
 router.post("/", (req, res) => {
     const bodyData = req.body; //배열인지 문자인지 구분을 못하기 때문에, 배열을 객체 형태로 쓰기 위해 JSON 형식을 쓰고, 그를 쓰기 위해서 parsing을 해줘야한다
+    function getId(){return parseInt(bodyData.id)}
+    const columns = mysql.raw('cartData.id as orderId, prodName, price, category, count, menuData.Id as id')
+    const condition = mysql.raw('menuData.id = cartData.prodId')
     db.query(
-        `INSERT INTO cartData (prodId) VALUES(${bodyData.id})`, (err, rows) => {
-            db.query(
-                `SELECT cartData.id as orderId, prodName, price, category, count, menuData.Id as id FROM menuData JOIN cartData on menuData.id = cartData.prodId where prodId = ${bodyData.id}`,
+        `INSERT INTO cartData (prodId) VALUES(?)`, [getId()], (err, rows) => {
+            const query = db.query(
+                `SELECT ? FROM menuData JOIN cartData on ? where prodId = ?`,
+                [columns, condition, getId()],
                 (err, orderedItem) => {
                     res.json({ 'orderedItem': orderedItem[0] }); // 배열 형태로 가져오기 때문에 보낼때는 가장 첫번째 아이템만 보내준다
                 }
@@ -38,9 +50,10 @@ router.post("/", (req, res) => {
 
 router.put("/", (req, res) => {
     const bodyData = req.body;
-    db.query(
-        `UPDATE cartData SET count = ${bodyData.count ? parseInt(bodyData.count) : "count+1"
-        } WHERE prodId = ${bodyData.prodId}`,
+    const condition1 = `${bodyData.count ? parseInt(bodyData.count) : "count+1"}`
+    const condition2 = `${bodyData.prodId}`
+    const query = db.query(
+        `UPDATE cartData SET count = ? WHERE prodId = ?`, [+condition1, +condition2],
         (err, rows) => {
             res.send({ status: 200 });
         }
--- routes/order.js
+++ routes/order.js
@@ -1,18 +1,22 @@
 var express = require('express');
+var mysql      = require('mysql');
 var router = express.Router();
 const db = require("../db.js");
 
 router.post("/", (req, res) => {
     const bodyData = req.body;
     const values = bodyData.map(({count, id}) => `(${count === null ? 1 : count}, ${id})`).join(",");
-    db.query(
-        `INSERT INTO orderData (count, prodId) VALUES${values}`,
+    const table1 = mysql.raw('orderData')
+    const table2 = mysql.raw('cartData')
+    const columns = ["count", "prodId"]
+    const sql = `INSERT INTO ? (??) VALUES${values}`;
+    const preparedQuery = mysql.format(sql, [table1, columns]);
+    const query = db.query(
+        preparedQuery,
         (err, rows) => {
-            console.log(`INSERT INTO orderData (count, prodId) VALUES${values}`)
             const orderIdArray = bodyData.map(({ orderId }) => `id = ${orderId}`).join(" or ");
-            console.log(orderIdArray)
-            db.query(
-                `DELETE FROM cartData`
+            const query = db.query(
+                `DELETE FROM ?`, table2
             );
             res.send({ status: 200 });
         }
@@ -22,9 +26,17 @@ router.post("/", (req, res) => {
 
 router.get("/history", (req, res) => {
     //주문 이후 주문한 데이터를 가져오는 라우터
-    db.query(`SELECT prodId, prodName, count, image, category, price, date_format(orderDate, '%d/%m/%y %T') as orderDate from orderData join menuData on orderData.prodId = menuData.id ORDER BY orderDate DESC`, (err, rows) => {
+    const columns = ["prodId", "prodName", "count", "image", "category", "price"]
+    const table1 = mysql.raw('orderData')
+    const table2 = mysql.raw('menuData')
+    const condition1 = mysql.raw("date_format(orderDate, '%d/%m/%y %T') as orderDate")
+    const condition2 = mysql.raw('orderData.prodId = menuData.id')
+    const sql = `SELECT ??, ? from ? join ? on ? ORDER BY orderDate DESC`;
+    const preparedQuery = mysql.format(sql, [columns, condition1, table1, table2, condition2]);
+    const query = db.query(preparedQuery, (err, rows) => {
         res.send(rows);
     });
+    console.log(query.sql)
 });
 
 
