83192b59bb58e5aefcc1105fc21ee9a44d0cb82a
hannah-developer <hannahchoi.developer@gmail.com>
2022-01-04 19:15:37 +0000

    refactoring setup: js and image files

---
+++ src/legacy/menuData.js
@@ -0,0 +1,108 @@
+export const menuData = {
+    menu: ["LOOSE TEA", "TEA BAG", "GIFT BOX"],
+    list: [{
+        name: "Alishan Jingsyuan",
+        price: 18.00,
+        category: 0,
+        id: 1
+
+    },
+    {
+        name: "Lishan Oolong",
+        price: 45.00,
+        category: 0,
+        id: 2
+    },
+    {
+        name: "Tie Guan Yin",
+        price: 25.00,
+        category: 0,
+        id: 3
+    },
+    {
+        name: "Roasted Shanlinsi Oolong",
+        price: 28.00,
+        category: 0,
+        id: 4
+    },
+    {
+        name: "Premium Jasmine Green Tea",
+        price: 15.00,
+        category: 0,
+        id: 5
+    },
+    {
+        name: "Oriental Beauty",
+        price: 24.00,
+        category: 0,
+        id: 6
+    },
+    {
+        name: "Aged Tie Guan Yin",
+        price: 35.00,
+        category: 0,
+        id: 7
+    },
+    {
+        name: "Aged Dongding Oolong",
+        price: 42.00,
+        category: 0,
+        id: 8
+    },
+    {
+        name: "Si Ji Chun",
+        price: 5.00,
+        category: 1,
+        id: 9
+    },
+    {
+        name: "Alishan Jingsyuan",
+        price: 5.00,
+        category: 1,
+        id: 10
+    },
+    {
+        name: "Lightly Roasted DongDing Oolong",
+        price: 5.00,
+        category: 1,
+        id: 11
+    },
+    {
+        name: "Tie Guan Yin",
+        price: 5.00,
+        category: 1,
+        id: 12
+    },
+    {
+        name: "Premium Jasmine Green Tea",
+        price: 5.00,
+        category: 1,
+        id: 13
+    },
+    {
+        name: "Guei Fei Tea",
+        price: 5.00,
+        category: 1,
+        id: 14
+    },
+
+    {
+        name: "Alishan Oolong + Premium Jasmine Green Tea",
+        price: 40.00,
+        category: 2,
+        id: 15
+    },
+    {
+        name: "Si ji Chun + Lightly roasted Oolong Tea",
+        price: 30.00,
+        category: 2,
+        id: 16
+    },
+    {
+        name: "Lightly Roasted Oolong Tea + Premium Jasmine Green Tea",
+        price: 35.00,
+        category: 2,
+        id: 17
+    }
+    ]
+}
---
+++ src/legacy/menuList.js
@@ -0,0 +1,59 @@
+class MenuList {
+    constructor() {
+        this.$nav = document.querySelector(".ulNav"); //반복적으로 변수에 할당을 하고싶지 않을때, 전역변수 대신에 이곳에 선언을 하는 것(과부하 방지)
+        this.$menuList = document.querySelector(".menuList");
+        //this.data = menuData
+        this.selectTab = null;
+        this.data = null;
+        this.navRender();
+        this.productLoad();
+    }
+
+    productLoad(menuID = 0) {
+        fetch(`product?menuId=${menuID}`, {
+            method: 'get',
+            headers: {
+                "Accept": "application/json",
+                "Content-type": "application/json; charset = UTF-8"
+            }
+        }
+        )
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("HTTP error " + res.status);
+                }
+                return res.json();
+            })
+            .then(data => {
+                this.setState(menuID, data);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+    setState(selectTab, data) {
+        //app.js에서 데이터를 받아서 데이터를 갱신해주는 용도
+        //상태를 변경해주는 함수
+        this.selectTab = selectTab;
+        this.data = data;
+        this.navRender();
+        this.listRender();
+    }
+
+    navRender() {
+        //html요소를 직접적으로 그려주는 함수
+        const menuData = ["LOOSE TEA", "TEA BAG", "GIFT BOX"];
+        this.$nav.innerHTML = menuData
+            .map((menu, index) => `<li data-index="${index}" class="${index === parseInt(this.selectTab) ? "active" : ""}" data-key="navItem">${menu}</li>`).join("");
+    }
+    //+ 기호를 앞에 붙이면 parseInt처럼 사용가능
+    listRender() {
+        let selectTab = this.selectTab; //클릭한 li의 인덱스
+        let list = this.data;
+
+        this.$menuList.innerHTML = list.map((item, index) => `<li data-key="menuItem"><img data-key="menuItem" data-id="${item.id}" src="/images/${item.image}.webp"><span class="prodName">${item.prodName}</span><span class="prodPrice">£ ${item.price.toFixed(2)}</span></li>`)
+            .join("");
+    }
+}
+export default MenuList;
